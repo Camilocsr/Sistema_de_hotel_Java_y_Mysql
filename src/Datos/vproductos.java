dc9eaa92b0489436aa25c239f1b21f9c3af7c75b
Camilocsr <esmunred@gmail.com>
2022-10-27 20:49:31 -0500

    Ingreso validacion de productos

---
+++ src/Datos/vproductos.java
@@ -0,0 +1,69 @@
+package Datos;
+
+/**
+ *
+ * @author /CSR/ Camilo Solano Rodriguez.
+ */
+public class vproductos {
+    private int idproducto;
+    private String nombre;
+    private String descripcion;
+    private String unidad_medida;
+    private Double precio_venta;
+    
+    
+    public vproductos() {
+    }
+    
+    
+    public vproductos(int idproducto, String nombre, String descripcion, String unidad_medida, Double precio_venta) {
+        this.idproducto = idproducto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.unidad_medida = unidad_medida;
+        this.precio_venta = precio_venta;
+    }
+    
+    
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUnidad_medida() {
+        return unidad_medida;
+    }
+
+    public void setUnidad_medida(String unidad_medida) {
+        this.unidad_medida = unidad_medida;
+    }
+
+    public Double getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(Double precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+    
+    
+}
